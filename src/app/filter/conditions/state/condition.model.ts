8d1ed8d1748c9456666a4296b85e3a6ccb0c8d40
thejsdeveloper <thejsdeveloper@gmail.com>
2019-10-17 09:28:17 +0800

    2 files modified

---
+++ src/app/filter/conditions/state/condition.model.ts
@@ -0,0 +1,7 @@
+import { UserDetails } from '../../user-details/state/user-details.model';
+
+export type Condition = {
+  code: string;
+  name: string;
+  userDetails: UserDetails[];
+};
---
+++ src/app/filter/user-details/state/user-details.model.ts
@@ -0,0 +1,8 @@
+
+
+export type UserDetails = {
+  
+  code: string;
+  name: string;
+  
+}
