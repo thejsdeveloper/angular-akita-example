15bb41f0fa640714e8e8efca540186d721ade317
thejsdeveloper <thejsdeveloper@gmail.com>
2019-10-18 14:27:08 +0800

    2 files modified

--- src/app/filter/conditions/state/condition.store.ts
+++ src/app/filter/conditions/state/condition.store.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@angular/core';
-import { EntityState, EntityStore, StoreConfig, MultiActiveState, EntityUIStore } from '@datorama/akita';
+import { Injectable } from "@angular/core";
+import {
+  EntityState,
+  EntityStore,
+  StoreConfig,
+  MultiActiveState,
+  EntityUIStore
+} from "@datorama/akita";
 
+import { Condition, ConditionUI } from "./condition.model";
 
-import { Condition, ConditionUI } from './condition.model';
-
-export interface ConditionState extends EntityState<Condition>,  MultiActiveState {}
+export interface ConditionState
+  extends EntityState<Condition>,
+    MultiActiveState {}
 export interface ConditionUIState extends EntityState<ConditionUI> {}
 const initialState = {
   active: []
 };
 
-@Injectable({ providedIn: 'root' })
-@StoreConfig({ name: 'conditions', idKey: 'code' })
+@Injectable({ providedIn: "root" })
+@StoreConfig({
+  name: "conditions",
+  idKey: "code",
+  cache: {
+    ttl: 3600000
+  }
+})
 export class ConditionStore extends EntityStore<ConditionState, Condition> {
   ui: EntityUIStore<ConditionUIState, ConditionUI>;
 
   constructor() {
     super(initialState);
-    this.createUIStore({}, {  resettable: true });
+    this.createUIStore({}, { resettable: true });
   }
 }
--- src/app/filter/filter.service.ts
+++ src/app/filter/filter.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from "@angular/core";
 import { FilterDataService } from "./filter-data.service";
 import { ConditionStore } from "./conditions/state/condition.store";
+import { ConditionQuery } from "./conditions/state/conditin.query";
 import { UserDetailsStore } from "./user-details/state/user-details.store";
+
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class FilterService {
   constructor(
     private filterDataService: FilterDataService,
     private conditionStore: ConditionStore,
+    private conditionQuery: ConditionQuery,
     private userDetailsStore: UserDetailsStore
   ) {}
 
   getFilterConditions() {
-    this.filterDataService.getFilterData().subscribe(data => {
-      this.conditionStore.set(data.entities.providers);
-      this.userDetailsStore.set(data.entities.userDetails);
-    });
+    if (!this.conditionQuery.getHasCache()) {
+      this.filterDataService.getFilterData().subscribe(data => {
+        this.conditionStore.set(data.entities.providers);
+        this.userDetailsStore.set(data.entities.userDetails);
+      });
+    }
   }
 
   toggleActiveState(id: string) {
@@ -28,7 +34,7 @@ export class FilterService {
   }
 
   updateUIStore(userDetails: string[]) {
-    this.conditionStore.ui.set( {
+    this.conditionStore.ui.set({
       userDetails
     });
   }
