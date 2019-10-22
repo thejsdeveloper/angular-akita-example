c7ea9dcc48a20df01852b2602794659c39141bf5
thejsdeveloper <thejsdeveloper@gmail.com>
2019-10-22 11:45:30 +0800

    1 file modified

--- src/app/filter/conditions/conditions.component.ts
+++ src/app/filter/conditions/conditions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { Observable, combineLatest } from "rxjs";
-import { filter, map, tap, auditTime} from "rxjs/operators";
+import { combineQueries } from '@datorama/akita';
+import { filter, map, tap } from "rxjs/operators";
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { FilterService } from "../../filter/filter.service";
 import { Condition } from "./state/condition.model";
@@ -35,13 +36,22 @@ export class ConditionsComponent implements OnInit {
   }
 
   updateUIStore() {
+ 
+    // this.conditionQuery.getActiveUserDetails()
+    //     .pipe(filter((data: string[]) => !!data.length)).subscribe(
+    //       activeUserDetails => this.filterService.updateUIStore(activeUserDetails)
+    //     )
+
 
     combineLatest(
-      this.conditionQuery.getActiveUserDetails(),
+      this.conditionQuery.getActiveUserDetails()
+        .pipe(filter((data: string[]) => !!data.length)),
+
       this.conditionQuery.ui.selectAll().pipe(map((data: string[]) => data.flat()))
+
     ).pipe(untilDestroyed(this)).subscribe(
       ([activeUserDetails, storedUserDetails]: [string[], string[]]) => {
-        
+
         if (activeUserDetails.length !== storedUserDetails.length) {
           
           this.filterService.updateUIStore(activeUserDetails);
